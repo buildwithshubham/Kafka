1a53d3ff093da3daa8215ef18b74fbda538c14cd
buildwithshubham <shubhamkadaskar84@gmail.com>
2026-06-17 22:56:42 +0530

    order updated

--- core/src/main/java/com/shubham/core/dto/Order.java
+++ core/src/main/java/com/shubham/core/dto/Order.java
@@ -1,5 +1,7 @@
 package com.shubham.core.dto;
 
+import com.shubham.core.types.OrderStatus;
+
 import java.util.UUID;
 
 public record Order(UUID orderId,
---
+++ credit-card-processor-service/src/main/java/com/shubham/credit_card_processor_service/CreditCardController.java
@@ -0,0 +1,21 @@
+package com.shubham.credit_card_processor_service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("ccp")
+public class CreditCardController {
+
+    private static final Logger LOGGER= LoggerFactory.getLogger(CreditCardController.class);
+
+    @PostMapping("/process")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void processCreditCard(@RequestBodyrequest){
+        LOGGER.info("Processing request : {}",request);
+    }
+
+}
